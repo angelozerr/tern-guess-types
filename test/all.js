af549e3c62db4ccebaed999da0f523490f3718ad
angelozerr <angelo.zerr@gmail.com>
2015-06-08 18:24:27 +0200

    Fix https://github.com/angelozerr/tern-guess-types/issues/1

--- test/all.js
+++ test/all.js
@@ -1,4 +1,5 @@
-exports['test guess types for function'] = require('./guesstypes_fn.js');
-exports['test guess types for function of member'] = require('./guesstypes_member_fn.js');
+exports['test guess types for function'] = require('./guesstypes_fn');
+exports['test guess types for function of member'] = require('./guesstypes_member_fn');
+exports['test guess types for function with multi types'] = require('./guesstypes_multitypes');
 
 if (require.main === module) require("test").run(exports);
---
+++ test/guesstypes_multitypes.js
@@ -0,0 +1,16 @@
+var util = require("./util");
+
+exports['test String#replace '] = function() {
+    
+  util.assertGuessTypes(
+    "var s1 = '', s2 = '', r1 = new RegExp('');" +
+	"var s3 = s1.",
+	"replace",
+	{
+	 "args": ["string|RegExp","string"],
+	 "string": ["s1","s2"],
+	 "RegExp": ["r1"]
+	}, [ "ecma5"]);  
+}
+
+if (module == require.main) require('test').run(exports)
